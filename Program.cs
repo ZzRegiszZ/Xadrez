a857e888855af6673a278f42bd4057cfd8a6c0bc
ZzRegiszZ <rct.araujo@hotmail.com>
2023-08-24 00:19:39 -0300

    teste

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using tabuleiro;
+
+namespace xadrez_console {
+    class Program {
+        static void Main(string[] args) {
+            posicao P;
+            P = new posicao(3,4);
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine( );
+        }
+    }
+}
