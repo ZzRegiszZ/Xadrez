fdd4aa6cf618f7dff278f8f404d003e80ed066af
ZzRegiszZ <rct.araujo@hotmail.com>
2023-08-26 00:20:11 -0300

    nova alteracao

--- Program.cs
+++ Program.cs
@@ -5,14 +5,12 @@ using tabuleiro;
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
-            posicao P;
-            P = new posicao(3,4);
+            Posicao P;
+            P = new Posicao(3,4);
             Console.WriteLine("Posição: " + P);
 
             Console.ReadLine( );
         }
     }
 }
-// comentáriossss
 
-//tess
